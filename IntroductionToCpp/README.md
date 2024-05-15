0547964fa30fe790d4a3dcdf93d2a795e4c4234f
Tamal <ckbtamaldipnew@gmail.com>
2024-05-15 20:12:29 +0600

    Variables and Datatype doen

---
+++ IntroductionToCpp/README.md
@@ -0,0 +1,10 @@
+## Important notes for CPP basics
+
+# variables and datatype:
+
+Number range in a variable : (-2^byte - 1) to (2^byte - 1)
+Here, byte is basically the occupied place of the variable
+
+let n = byte
+for integer, signed form : 2^(n-1) to 2^(n-1) - 1
+for integer, unsigned form : 0 to 2^n - 1
---
+++ IntroductionToCpp/Variables&Datatype.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello there!" << endl;
+
+    // variables define
+    int age = 25;
+    cout << "My age is : " << age << endl;
+
+    // declaration
+    // int hi;
+    // cout << hi << endl;
+
+    // updating
+    age = 101;
+    cout << "Updated age is : " << age << endl;
+
+    //! DATATYPES :
+
+    int count = 5;
+    float share = 3.14;
+    char alphabet = 'z';
+    double weight = 55.351564894;
+    bool logic = true;
+
+    cout << count << endl;
+    cout << share << endl;
+    cout << alphabet << endl;
+    cout << weight << endl;
+    cout << logic << endl;
+
+    return 0;
+}
