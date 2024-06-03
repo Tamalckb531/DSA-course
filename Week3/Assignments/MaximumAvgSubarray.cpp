017a7d04eb4fff6cda1a773d82d588c591d3e601
Tamal <ckbtamaldipnew@gmail.com>
2024-06-03 19:44:45 +0600

    Forgot to add the code for last one :) Now added

--- Week3/Assignments/MaximumAvgSubarray.cpp
+++ Week3/Assignments/MaximumAvgSubarray.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
+
+double bruteForce(vector<int> &nums, int &k)
+{
+    int maxSum = INT_MIN;
+    int i = 0;
+    int j = k - 1;
+
+    while (j < nums.size())
+    {
+        int sum = 0;
+        for (int y = i; y <= j; y++)
+        {
+            sum += nums[y];
+        }
+        maxSum = max(maxSum, sum);
+        j++;
+        i++;
+    }
+
+    double maxAvg = maxSum / (double)k;
+    return maxAvg;
+}
+
+double slidingWindow(vector<int> &nums, int &k)
+{
+    int i = 0;
+    int j = k - 1;
+    int sum = 0;
+
+    // initial sum
+    for (int y = i; y <= j; y++)
+    {
+        sum += nums[y];
+    }
+    int maxSum = sum; //? For comparison
+    j++;              //? For adding the next element in the next loop
+
+    while (j < nums.size())
+    {
+        sum = sum - nums[i];       // removing first element
+        sum = sum + nums[j];       // adding next element
+        maxSum = max(maxSum, sum); // Getting the larger sum
+        // sliding the window
+        i++;
+        j++;
+    }
+
+    double maxAvg = maxSum / (double)k;
+    return maxAvg;
+}
 
 int main()
 {
