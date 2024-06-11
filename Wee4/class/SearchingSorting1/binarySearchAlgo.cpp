760b9750a9d544e274e69ce147a55bf844a0ea5d
Tamal <ckbtamaldipnew@gmail.com>
2024-06-11 11:32:37 +0600

    add stl of binary_search

--- Wee4/class/SearchingSorting1/binarySearchAlgo.cpp
+++ Wee4/class/SearchingSorting1/binarySearchAlgo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int search(vector<int> &nums, int target)
 {
@@ -22,5 +23,14 @@ int search(vector<int> &nums, int target)
 
 int main()
 {
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    int n = 9;
+    int target = 70;
+    int target2 = 700;
+
+    bool ans = binary_search(arr, arr + n, target);
+    bool ans2 = binary_search(arr, arr + n, target2);
+    cout << ans << endl;
+    cout << ans2 << endl;
     return 0;
 }
