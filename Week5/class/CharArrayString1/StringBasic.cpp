e7eeeda5bbcfaab7f64859575cb4e24507b21ea8
Tamal <ckbtamaldipnew@gmail.com>
2024-07-09 10:09:49 +0600

    Week 5 (class) -> Char Arrays & Strings 1 -> Important Note Of String

--- Week5/class/CharArrayString1/StringBasic.cpp
+++ Week5/class/CharArrayString1/StringBasic.cpp
@@ -5,13 +5,44 @@ using namespace std;
 int main()
 {
     //? creation
-    string str;
+    // string str;
 
-    cout << "enter the input" << endl;
-    cin >> str;
+    // cout << "enter the input" << endl;
+    // cin >> str;
 
-    cout << "Str : " << str << endl;
-    cout << str[0] << endl;
+    // cout << "Str : " << str << endl;
+    // cout << str[0] << endl;
+
+    //? input with delimeter
+
+    // string sentence;
+    // getline(cin, sentence, '\n');
+    // cout << sentence << endl;
+
+    //? string built in functions
+
+    // string name = "Chatrapati Shivaji Mahraj";
+    // string word = "Mahraj";
+    // cout << name[0] << endl;
+    // cout << name.front() << endl;
+    // cout << name.back() << endl;
+    // cout << name.length() << endl
+    //  << endl
+    //  << endl;
+
+    // auto it = name.begin();
+
+    // while (it != name.end())
+    // {
+    //     cout << *it << endl;
+    //     it++;
+    // }
+
+    // cout << name.substr(5, 5) << endl;
+    // cout << name.substr(6) << endl;
+
+    // int ans = name.find(word);
+    // cout << ans << endl; // return the starting index of the word found in name.
 
     return 0;
 }
--- Week5/readme.md
+++ Week5/readme.md
@@ -6,3 +6,12 @@
 
 1. cin input will stop taking input if we hit the space. It will still take input but it won't store the character after space as space is considered as terminator.
 2. To take input with space : cin.getline(arr, size, delimiter) -> space is default delimiter in cin, but with getline we can give out custom delimiter.
+
+**Important Note Of String:**
+
+1. string is like vector of char.
+2. .front() -> to get the first char of a string.
+3. .back() -> to get the last char of a string.
+4. .begin/end() -> to get the first/last memory location of a string.
+5. .find() -> to find a specific word in a string.
+6. .substr() -> to get a part of the string index wise. It takes two parameter. First is the "starting index", second is the "how much from starting index". If we don't give the second parameter, it will return us all the char till end form starting index.
