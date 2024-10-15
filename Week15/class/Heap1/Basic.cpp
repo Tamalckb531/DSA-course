e68a3e823661a99a1e5da4600d1662a980f9d507
Tamal <ckbtamaldipnew@gmail.com>
2024-10-16 00:53:31 +0600

    Week 14 (class) -> Heap 1  -> Basic Heap

---
+++ Week15/class/Heap1/Basic.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Heap
+{
+public:
+    int *arr;
+    int capacity;
+    int index;
+
+    Heap(int capacity)
+    {
+        this->capacity = capacity;
+        arr = new int[capacity];
+        index = 0;
+
+        //? will increase index while inserting
+    }
+
+    void printHeap()
+    {
+        for (int i = 0; i < capacity; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void insert(int val)
+    {
+        if (index == capacity - 1)
+        {
+            cout << "Overflow" << endl;
+            return;
+        }
+
+        index++;
+        arr[index] = val;
+
+        int currIndex = index;
+        while (currIndex > 1)
+        {
+            int parentIndex = currIndex / 2;
+
+            if (arr[parentIndex] < arr[currIndex])
+            {
+                swap(arr[parentIndex], arr[currIndex]);
+                currIndex = parentIndex;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    void deleteFromHeap()
+    {
+        swap(arr[1], arr[index]);
+        index--;
+        heapify(arr, index, 1);
+    }
+
+    void heapify(int *arr, int n, int currIndex)
+    {
+        int i = currIndex;
+        int leftIndex = 2 * i;
+        int rightIndex = 2 * i + 1;
+
+        int indexOfLargest = i;
+
+        //? check for left
+
+        if (leftIndex < n && arr[leftIndex] > arr[indexOfLargest])
+        {
+            indexOfLargest = leftIndex;
+        }
+
+        //? check for left
+
+        if (rightIndex < n && arr[rightIndex] > arr[indexOfLargest])
+        {
+            indexOfLargest = rightIndex;
+        }
+
+        if (indexOfLargest != i)
+        {
+            swap(arr[i], arr[indexOfLargest]);
+            i = indexOfLargest;
+            heapify(arr, n, i);
+        }
+    }
+};
+
+int main()
+{
+
+    Heap pq(10);
+
+    pq.insert(10);
+    pq.printHeap();
+    pq.insert(20);
+    pq.insert(30);
+    pq.insert(40);
+    pq.insert(35);
+    pq.printHeap();
+
+    pq.deleteFromHeap();
+    pq.printHeap();
+
+    return 0;
+}
---
+++ Week15/readme.md
@@ -0,0 +1,15 @@
+# Week 15 (class):
+
+## Heap 1
+
+Read heapify algo more
+
+## Heap 2
+
+## Heap 3
+
+# Week 15 (Mega class):
+
+**LeetCode n. x [problem](link)**
+
+# Week 15 (assignment):
