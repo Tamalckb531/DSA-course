58edd1b1aa7f97ef0cc3f651062004460b1fe8ce
Tamal <ckbtamaldipnew@gmail.com>
2024-10-17 22:49:57 +0600

    Week 14 (class) -> Heap 1  -> HeapSort

--- Week15/class/Heap1/Basic.cpp
+++ Week15/class/Heap1/Basic.cpp
@@ -59,55 +59,84 @@ public:
     {
         swap(arr[1], arr[index]);
         index--;
-        heapify(arr, index, 1);
-    }
-
-    void heapify(int *arr, int n, int currIndex)
-    {
-        int i = currIndex;
-        int leftIndex = 2 * i;
-        int rightIndex = 2 * i + 1;
-
-        int indexOfLargest = i;
-
-        //? check for left
-
-        if (leftIndex < n && arr[leftIndex] > arr[indexOfLargest])
-        {
-            indexOfLargest = leftIndex;
-        }
-
-        //? check for left
-
-        if (rightIndex < n && arr[rightIndex] > arr[indexOfLargest])
-        {
-            indexOfLargest = rightIndex;
-        }
-
-        if (indexOfLargest != i)
-        {
-            swap(arr[i], arr[indexOfLargest]);
-            i = indexOfLargest;
-            heapify(arr, n, i);
-        }
+        // heapify(arr, index, 1);
     }
 };
+
+void heapify(int *arr, int n, int currIndex)
+{
+    int i = currIndex;
+    int leftIndex = 2 * i;
+    int rightIndex = 2 * i + 1;
+
+    int indexOfLargest = i;
+
+    //? check for left
+
+    if (leftIndex < n && arr[leftIndex] > arr[indexOfLargest])
+    {
+        indexOfLargest = leftIndex;
+    }
+
+    //? check for left
+
+    if (rightIndex < n && arr[rightIndex] > arr[indexOfLargest])
+    {
+        indexOfLargest = rightIndex;
+    }
+
+    if (indexOfLargest != i)
+    {
+        swap(arr[i], arr[indexOfLargest]);
+        i = indexOfLargest;
+        heapify(arr, n, i);
+    }
+}
+
+void buildHeap(int *arr, int n)
+{
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+}
+
+void heapSort(int *arr, int n)
+{
+    int e = n - 1;
+    while (e > 1)
+    {
+        swap(arr[1], arr[e]);
+        e--;
+        heapify(arr, e + 1, 1);
+    }
+}
 
 int main()
 {
 
-    Heap pq(10);
+    // Heap pq(10);
 
-    pq.insert(10);
-    pq.printHeap();
-    pq.insert(20);
-    pq.insert(30);
-    pq.insert(40);
-    pq.insert(35);
-    pq.printHeap();
+    int arr[] = {-1, 10, 20, 30, 40, 50};
+    int n = 6;
 
-    pq.deleteFromHeap();
-    pq.printHeap();
+    buildHeap(arr, n);
+    for (int i = 1; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // pq.insert(10);
+    // pq.printHeap();
+    // pq.insert(20);
+    // pq.insert(30);
+    // pq.insert(40);
+    // pq.insert(35);
+    // pq.printHeap();
+
+    // pq.deleteFromHeap();
+    // pq.printHeap();
 
     return 0;
 }
