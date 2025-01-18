bf27264747d3514b9d73a9921bf848e33ef90ed9
Tamal <ckbtamaldipnew@gmail.com>
2025-01-18 20:26:04 +0600

    Contest -> LC_BiWeekly -> 147 -> leetcode 3408. Design Task Manager

---
+++ Contest/LC_BiWeekly/Cnt147/TaskManager.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+using namespace std;
+class TaskManager
+{
+public:
+    map<int, pair<int, int>> mp; //? taskId -> {userId, priority} mapping
+    set<pair<int, int>> st;      //? {priority, taskId} stack store
+    TaskManager(vector<vector<int>> &tasks)
+    {
+        for (auto it : tasks)
+        {
+            st.insert({it[2], it[1]});
+            mp[it[1]] = {it[0], it[2]};
+        }
+    }
+
+    void add(int userId, int taskId, int priority)
+    {
+        st.insert({priority, taskId});
+        mp[taskId] = {userId, priority};
+    }
+
+    void edit(int taskId, int newPriority)
+    {
+        pair<int, int> p = mp[taskId]; //? have {userId, priority}
+
+        //? set new in map
+        mp[taskId] = {p.first, newPriority};
+
+        //? delete old and set new in stack
+        pair<int, int> oldPair = {p.second, taskId};
+        auto it = st.find(oldPair);
+        st.erase(it);
+        st.insert({newPriority, taskId});
+    }
+
+    void rmv(int taskId)
+    {
+        pair<int, int> p = mp[taskId];
+
+        //? remove in map
+        mp.erase(taskId);
+
+        //? remove in stack
+        pair<int, int> oldPair = {p.second, taskId};
+        auto it = st.find(oldPair);
+        st.erase(it);
+    }
+
+    int execTop()
+    {
+        //? safety check
+        if (mp.size() == 0)
+            return -1;
+
+        //? get the userId of task with the highest priority
+        auto it = st.end();
+        it--;
+        pair<int, int> p = *it;
+        int ans = mp[p.second].first; //? userId
+
+        //? remove the taskId form both set and map
+        mp.erase(p.second);
+        st.erase(it);
+
+        //? return the userId
+        return ans;
+    }
+};
--- Contest/LC_BiWeekly/readme.md
+++ Contest/LC_BiWeekly/readme.md
@@ -1,3 +1,4 @@
 #Contest 147
 
 **LeetCode 3407. Substring Matching Pattern[problem](https://leetcode.com/problems/substring-matching-pattern/)**
+**LeetCode 3408. Design Task Manager[problem](https://leetcode.com/problems/design-task-manager/description/)**
