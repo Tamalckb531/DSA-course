effee4a2bca02749e3b23c2c7f368e3ae2322419
Tamal <ckbtamaldipnew@gmail.com>
2025-01-22 16:19:48 +0600

    Week3 -> Array2 -> pair print -> Practice done

--- Week3/class/Array2/pairPrint.cpp
+++ Week3/class/Array2/pairPrint.cpp
@@ -13,12 +13,24 @@ void pairPrint(int nums[], int size)
     }
 }
 
+void pairPrintPrac(int nums[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << "(" << nums[i] << "," << nums[j] << ")" << "  ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40};
     int size = 4;
 
-    pairPrint(arr, size);
+    pairPrintPrac(arr, size);
 
     cout << endl;
     return 0;
