3efbf3ca11fb90d61cc2ac7b17f2e18562859ca8
Tamal <ckbtamaldipnew@gmail.com>
2025-01-22 16:35:23 +0600

    Week3 -> Array2 -> twoSum -> Practice done

--- Week3/class/Array2/twoSum.cpp
+++ Week3/class/Array2/twoSum.cpp
@@ -17,13 +17,28 @@ void PrintTwoSum(int nums[], int size, int target)
     }
 }
 
+void PrintTwoSumPrac(int nums[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                cout << "(" << nums[i] << "," << nums[j] << ")" << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40};
     int size = 4;
     int target = 60;
 
-    PrintTwoSum(arr, size, target);
+    PrintTwoSumPrac(arr, size, target);
 
     cout << endl;
     return 0;
