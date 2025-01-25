f70ae4bf6a773ac7c61022ef305adfd22d0aa2b7
Tamal <ckbtamaldipnew@gmail.com>
2025-01-25 17:39:54 +0600

    Week3 -> Array Assignment -> GFG: KeyPair  -> Practice done

--- Week3/Assignments/keyPair.cpp
+++ Week3/Assignments/keyPair.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 bool twoPointerKeyPair(int arr[], int n, int x)
@@ -25,6 +26,30 @@ bool hasArrayTwoCandidates(int arr[], int n, int x)
     // code here
     std::sort(arr, arr + n);
     return twoPointerKeyPair(arr, n, x);
+}
+
+bool twoPointerCheck(vector<int> &arr, int target)
+{
+
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low < high)
+    {
+        if (arr[low] + arr[high] == target)
+            return true;
+        else if (arr[low] + arr[high] < target)
+            low++;
+        else if (arr[low] + arr[high] > target)
+            high++;
+    }
+    return false;
+}
+
+bool twoSum(vector<int> &arr, int target)
+{
+    sort(arr.begin(), arr.end());
+    return twoPointerCheck(arr, target);
 }
 
 int main()
