cea2d7113f33ab75726c40ed65ecd06189f56baf
Tamal <ckbtamaldipnew@gmail.com>
2025-02-03 22:11:02 +0600

    Week3 -> Array Assignment -> GFG. Common elements  -> Practice done

--- Week3/Assignments/CommonElements.cpp
+++ Week3/Assignments/CommonElements.cpp
@@ -42,6 +42,54 @@ vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
     return ans;
 }
 
+vector<int> commonElements(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3)
+{
+    int arr1Size = arr1.size();
+    int arr2Size = arr2.size();
+    int arr3Size = arr3.size();
+
+    int arr1Index, arr2Index, arr3Index;
+    arr1Index = arr2Index = arr3Index = 0;
+
+    vector<int> ans;
+    set<int> temp;
+
+    while (arr1Index < arr1Size && arr2Index < arr2Size && arr3Index < arr3Size)
+    {
+        if (arr1[arr1Index] == arr2[arr2Index] && arr2[arr2Index] == arr3[arr3Index])
+        {
+            temp.insert(arr1[arr1Index]);
+            arr1Index++;
+            arr2Index++;
+            arr3Index++;
+        }
+        else if (arr1[arr1Index] < arr2[arr2Index])
+        {
+            arr1Index++;
+        }
+        else if (arr2[arr2Index] < arr3[arr3Index])
+        {
+            arr2Index++;
+        }
+        else
+        {
+            arr3Index++;
+        }
+    }
+
+    for (auto element : temp)
+    {
+        ans.push_back(element);
+    }
+
+    if (ans.empty())
+    {
+        ans.push_back(-1);
+    }
+
+    return ans;
+}
+
 int main()
 {
 
