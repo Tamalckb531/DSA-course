44c60abed5f79d54f5ad11c39edf2d64cd22128f
Tamal <ckbtamaldipnew@gmail.com>
2025-02-16 11:39:19 +0600

    Week3 -> Array Assignment -> Leetcode 54. Spiral Matrix -> Practice done

--- Week3/Assignments/SpiralPrint.cpp
+++ Week3/Assignments/SpiralPrint.cpp
@@ -56,6 +56,61 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
     return ans;
 }
 
+//? PROCESS : Starting Row -> Ending Column -> Ending Row -> Starting Column -> Starting Row.....
+vector<int> spiralOrderTest(vector<vector<int>> &matrix)
+{
+    int rowSize = matrix.size();
+    int colSize = matrix[0].size();
+
+    int totalElemCount = rowSize * colSize;
+    vector<int> ans;
+
+    //? pointer initialize:
+    int startingRow = 0;
+    int startingCol = 0;
+    int endingRow = rowSize - 1;
+    int endingCol = colSize - 1;
+
+    int count = 0;
+
+    while (count < totalElemCount)
+    {
+        //? get the element of Starting Row : SCol -> ECol
+        for (int i = startingCol; i <= endingCol && count < totalElemCount; i++)
+        {
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+        startingRow++;
+
+        //? get the element of Ending Column : SRow -> ERow
+        for (int i = startingRow; i <= endingRow && count < totalElemCount; i++)
+        {
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+        endingCol--;
+
+        //? get the element of Ending Row : ECol -> SCol
+        for (int i = endingCol; i >= startingCol && count < totalElemCount; i--)
+        {
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+        endingRow--;
+
+        //? get the element of Starting Column : ERow -> SRow
+        for (int i = endingRow; i >= startingRow && count < totalElemCount; i--)
+        {
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+        startingCol++;
+    }
+
+    return ans;
+}
+
 int main()
 {
     cout << endl;
