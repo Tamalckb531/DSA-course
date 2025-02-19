d145e50c8e1e5b18ab6a2a2aede8a3c4c6c4c2fb
Tamal <ckbtamaldipnew@gmail.com>
2025-02-19 09:53:39 +0600

    Week3 -> Array Assignment -> GFG: Factorials of large numbers -> Practice done

--- Week3/Assignments/BigFactorial.cpp
+++ Week3/Assignments/BigFactorial.cpp
@@ -28,6 +28,34 @@ vector<int> factorial(int N)
     return ans;
 }
 
+vector<int> factorialPrac(int n)
+{
+    int carry = 0;
+    vector<int> ans;
+    ans.push_back(1);
+
+    //? outer loop for -> how many numbers to multiply -> from 2 to n
+    for (int i = 2; i <= n; i++)
+    {
+        //? inner loop for -> how many digits to multiply -> from 0 to ans.size()
+        for (int j = 0; j < ans.size(); j++)
+        {
+            int num = ans[j] * i + carry;
+            ans[j] = num % 10;
+            carry = num / 10;
+        }
+        while (carry)
+        {
+            ans.push_back(carry % 10);
+            carry = carry / 10;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
+
 int main()
 {
     cout << endl;
