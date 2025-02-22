2f88e029207c59112026e95addfe8fd26c801e03
Tamal <ckbtamaldipnew@gmail.com>
2025-02-22 19:19:20 +0600

    Week4 -> Searching Sorting 1 -> LeetCode 34. Find First and Last Position of Element in Sorted Array. -> Practice done

--- Week4/class/SearchingSorting1/firstAndLastOccurence.cpp
+++ Week4/class/SearchingSorting1/firstAndLastOccurence.cpp
@@ -72,6 +72,73 @@ vector<int> searchRange(vector<int> &nums, int target)
     return temp;
 }
 
+void firstOccurencePrac(vector<int> &nums, int &firstOcc, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    int mid = end + (start - end) / 2;
+
+    while (start <= end)
+    {
+        if (nums[mid] == target)
+        {
+            firstOcc = mid;
+            end = mid - 1; //? search more in left for first occurence
+        }
+        else if (target > nums[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+        mid = end + (start - end) / 2;
+    }
+}
+
+void lastOccurencePrac(vector<int> &nums, int &lastOcc, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    int mid = end + (start - end) / 2;
+
+    while (start <= end)
+    {
+        if (nums[mid] == target)
+        {
+            lastOcc = mid;
+            start = mid + 1; //? search more in right for last occurence
+        }
+        else if (target > nums[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+        mid = end + (start - end) / 2;
+    }
+}
+
+vector<int> searchRangePrac(vector<int> &nums, int target)
+{
+    vector<int> temp;
+    int firstOcc = -1;
+    int lastOcc = -1;
+
+    firstOccurencePrac(nums, firstOcc, target);
+    lastOccurencePrac(nums, lastOcc, target);
+
+    temp.push_back(firstOcc);
+    temp.push_back(lastOcc);
+
+    return temp;
+}
+
 int main()
 {
     return 0;
