65c3138731e0b7220b90a8e638104c4f20723011
Tamal <ckbtamaldipnew@gmail.com>
2025-02-24 02:07:06 +0600

    Week4 -> Searching Sorting 2 -> LeetCode 74. Search a 2D Matrix  -> Practice done

--- Week4/class/SearchingSorting2/2DArraySearch.cpp
+++ Week4/class/SearchingSorting2/2DArraySearch.cpp
@@ -37,6 +37,34 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
     return 0;
 }
 
+bool searchMatrixPractice(vector<vector<int>> &matrix, int target)
+{
+    int row = matrix.size();
+    int col = matrix[0].size();
+    int start = 0;
+    int end = row * col - 1;
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        int rowIndex = mid / col;
+        int colIndex = mid % col;
+
+        int element = matrix[rowIndex][colIndex];
+
+        if (element == target)
+            return 1;
+        else if (element < target)
+            start = mid + 1;
+        else
+            end = mid - 1;
+
+        mid = start + (end - start) / 2;
+    }
+
+    return -1;
+}
+
 int main()
 {
     return 0;
