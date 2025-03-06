66539047dbe9cef374ee5dc214bae66befb5ed4c
Tamal <ckbtamaldipnew@gmail.com>
2025-03-06 20:33:34 +0600

    Week4 -> Mega Class: Searching and Sorting -> LeetCode 875. Koko Eating Bananas -> Practice done

--- Week4/class/MegaClassSS/kokoEatingBananas.cpp
+++ Week4/class/MegaClassSS/kokoEatingBananas.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 using namespace std;
+
+//? make a search space from 0 to max in piles for speed -> Do binary search on that -> validate the speed, if validate than store and compute for less speed -> if no validate than increase the speed
+
+//? Validate speed : Get the sum of total hour needed to complete each piles with the speed -> if it's less or equal to h than speed valid.
+
+bool validateSpeed(int speed, vector<int> &piles, int hour)
+{
+    long long totalHours = 0;
+
+    for (int i = 0; i < piles.size(); i++)
+    {
+        totalHours += ceil(piles[i] / (double)speed);
+    }
+
+    return totalHours <= hour;
+}
+
+int minEatingSpeedPractice(vector<int> &piles, int h)
+{
+    int start = 1;
+    auto it = max_element(piles.begin(), piles.end());
+    int end = *it;
+    int mid = start + (end - start) / 2;
+    int minimumSpeed = -1;
+
+    while (start <= end)
+    {
+        int speed = mid;
+        if (validateSpeed(speed, piles, h))
+        {
+            minimumSpeed = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+
+    return minimumSpeed;
+}
 
 bool speedValidator(vector<int> &piles, int hour, int speed)
 {
