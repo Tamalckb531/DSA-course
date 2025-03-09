7d25ec3824b207b4c63c1d338fb60bdde7ca36bf
Tamal <ckbtamaldipnew@gmail.com>
2025-03-09 22:19:59 +0600

    Week4 -> Assignments -> Leetcode 658. Find K Closest Elements -> Practice done

--- Week4/Assignments/KClosestElem.cpp
+++ Week4/Assignments/KClosestElem.cpp
@@ -22,6 +22,25 @@ vector<int> twoPointerMethod(vector<int> &arr, int k, int x)
     return vector<int>(arr.begin() + low, arr.begin() + high + 1);
 }
 
+vector<int> twoPointerMethodPractice(vector<int> &arr, int k, int x)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (high - low >= k)
+    {
+        int diffLeft = x - arr[low];
+        int diffRight = arr[high] - x;
+
+        if (diffLeft > diffRight)
+            low++;
+        else
+            high--; //? satisfy both diffRight bigger or equal condition
+    }
+
+    return vector<int>(arr.begin() + low, arr.begin() + high + 1);
+}
+
 vector<int> findClosestElements(vector<int> &arr, int k, int x)
 {
     return twoPointerMethod(arr, k, x);
