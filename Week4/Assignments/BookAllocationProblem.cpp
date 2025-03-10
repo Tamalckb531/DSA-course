5484089980be83363dcc274d9fab6d34ae7f86a1
Tamal <ckbtamaldipnew@gmail.com>
2025-03-10 22:09:10 +0600

    Week4 -> Assignments -> GFG. Allocate Minimum Pages -> Practice done

--- Week4/Assignments/BookAllocationProblem.cpp
+++ Week4/Assignments/BookAllocationProblem.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <numeric>
+#include <vector>
 using namespace std;
 
 bool isPossibleForAllocation(int arr[], int totalStudent, int totalBook, int minimumPage)
@@ -61,6 +62,66 @@ long long findPages(int n, int arr[], int m)
         {
             start = mid + 1;
         }
+        mid = start + (end - start) / 2;
+    }
+
+    return ans;
+}
+
+bool isPossibleToAllocatePractice(vector<int> &arr, int minPage, int totalStudent, int totalBooks)
+{
+    int pageSum = 0;
+    int studentCount = 1; //? 1st student to allocate books
+
+    for (int i = 0; i < totalBooks; i++)
+    {
+        if (arr[i] > minPage)
+            return false;
+
+        if (pageSum + arr[i] > minPage)
+        {
+            //? can't allocate to the current student -> shift to next student
+            studentCount++;
+            pageSum = arr[i]; //? new book allocated for the next student
+
+            if (studentCount > totalStudent)
+                return false; //? page still left after allocating to all studnets
+        }
+        else
+        {
+            pageSum += arr[i]; //? book pages allocation
+        }
+    }
+
+    return true; //? pages allocated to all the students
+}
+
+int findPagesPractice(vector<int> &arr, int k)
+{
+    int totalStudent = k;
+    int totalBooks = arr.size();
+
+    if (totalStudent > totalBooks)
+        return -1;
+
+    //? search space for the books
+    int start = 0;
+    int end = accumulate(arr.begin(), arr.end(), 0); //? sum up books array numbers | total pages
+    int mid = start + (end - start) / 2;             //? atmost page allocated to a student
+    int ans = 0;
+
+    while (start <= end)
+    {
+        if (isPossibleToAllocatePractice(arr, mid, totalStudent, totalBooks))
+        {
+            ans = mid;
+            end = mid - 1; //? as per question, we need the minimum value
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
         mid = start + (end - start) / 2;
     }
 
