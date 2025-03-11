341d90e7b5f3b0b7a877ad7dc4aa214731c13c1f
Tamal <ckbtamaldipnew@gmail.com>
2025-03-11 20:36:10 +0600

    Week4 -> Assignments -> GFG Aggressive Cows -> problem done

--- Week4/Assignments/AggresiveCows.cpp
+++ Week4/Assignments/AggresiveCows.cpp
@@ -59,6 +59,62 @@ int solve(int n, int k, vector<int> &stalls)
     return ans;
 }
 
+bool isCowPlacementPossible(vector<int> &stalls, int totalCows, int stallSize, int minimumDis)
+{
+    int cowCount = 1;
+    int cowPos = stalls[0]; //? first cow placed
+
+    for (int i = 1; i < stallSize; i++)
+    {
+
+        int nextCowPlacementDistance = stalls[i] - cowPos;
+
+        if (nextCowPlacementDistance >= minimumDis)
+        {
+            //? can place the cow
+            cowCount++;
+            cowPos = stalls[i]; //? newly cow placement position
+        }
+
+        if (cowCount == totalCows)
+            return true; //? all cow placed
+    }
+
+    return false; //? all cow not placed
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+
+    int totalCows = k;
+    int n = stalls.size();
+    sort(stalls.begin(), stalls.end()); //? sorted as we need the max distance for bianry search
+
+    //? search space for stall
+    int start = 0;
+    int end = stalls[n - 1] - stalls[0]; //? max distance
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (isCowPlacementPossible(stalls, totalCows, n, mid))
+        {
+            ans = mid;
+            start = mid + 1; //? search for the max
+        }
+        else
+        {
+            //? cow not placeed for over distance
+            end = mid - 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+
+    return ans;
+}
+
 int main()
 {
     return 0;
