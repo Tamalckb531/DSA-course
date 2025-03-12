fd6310adf3726585a8c4f4f316b60ec7023e60aa
Tamal <ckbtamaldipnew@gmail.com>
2025-03-12 19:19:42 +0600

    Week4 -> Mega Array -> LeetCode 48. Rotate Image -> problem done

--- Week4/class/MegaArray/rotateImageLeetCode48.cpp
+++ Week4/class/MegaArray/rotateImageLeetCode48.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void rotate(vector<vector<int>> &matrix)
@@ -18,6 +19,28 @@ void rotate(vector<vector<int>> &matrix)
 
     //? rotate the matrix with reverse:
     for (int i = 0; i < nrows; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+void rotatePractice(vector<vector<int>> &matrix)
+{
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+
+    //? transpose the matrix
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < columns; j++)
+        {
+            //? j starting from i as we swaping
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    //? reverse each array inside matrix
+    for (int i = 0; i < rows; i++)
     {
         reverse(matrix[i].begin(), matrix[i].end());
     }
