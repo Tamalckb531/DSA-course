2557d1b9068f5363f66781b6c4cc52300ddae01f
Tamal <ckbtamaldipnew@gmail.com>
2025-03-12 19:32:53 +0600

    Week4 -> Mega Array -> LeetCode 53. Maximum Subarray -> problem done

--- Week4/class/MegaArray/maximumSubarray.cpp
+++ Week4/class/MegaArray/maximumSubarray.cpp
@@ -20,6 +20,24 @@ int maxSubArray(vector<int> &nums)
     return ans;
 }
 
+//? kadane's algo -> store maxSum and drop sum to 0 when it reach -ve value
+int maxSubArrayPractice(vector<int> &nums)
+{
+    int maxSum = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        maxSum = max(sum, maxSum);
+
+        if (sum < 0)
+            sum = 0; //? dropping sum
+    }
+
+    return maxSum;
+}
+
 int main()
 {
     return 0;
