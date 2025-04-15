014cac7d349d764b623df55c8d7ba30baafc98a8
Tamal <ckbtamaldipnew@gmail.com>
2025-04-15 11:21:38 +0600

    Week5 -> Char Arrays & Strings 2 -> LeetCode 680. Valid Palindrome II -> Practice done

--- Week5/class/CharArrayString2/ValidPalindrome2.cpp
+++ Week5/class/CharArrayString2/ValidPalindrome2.cpp
@@ -43,6 +43,50 @@ bool validPalindrome(string s)
     return true;
 }
 
+bool checkPalin(string str, int left, int right)
+{
+    while (left <= right)
+    {
+        if (str[left] == str[right])
+        {
+            left++;
+            right--;
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+//? abccdba
+
+bool validPalindromePractice(string s)
+{
+    int len = s.length();
+    int left = 0;
+    int right = len - 1;
+    bool ans = true;
+
+    while (left <= right)
+    {
+        if (s[left] == s[right])
+        {
+            left++;
+            right--;
+        }
+        else
+        {
+            bool leftAns = checkPalin(s, left + 1, right);
+            bool rightAns = checkPalin(s, left, right - 1);
+            ans = leftAns || rightAns;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     return 0;
