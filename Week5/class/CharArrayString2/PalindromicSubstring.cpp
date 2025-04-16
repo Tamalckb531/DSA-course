3124c8778e5cc5082492b678a9eacc3f4d8a7834
Tamal <ckbtamaldipnew@gmail.com>
2025-04-16 21:33:32 +0600

    Week5 -> Char Arrays & Strings 2 -> LeetCode 647. Palindromic Substrings -> Practice done

--- Week5/class/CharArrayString2/PalindromicSubstring.cpp
+++ Week5/class/CharArrayString2/PalindromicSubstring.cpp
@@ -36,6 +36,45 @@ int countSubstrings(string s)
     return totalCount;
 }
 
+//? to find how many sequences of s from center is palindrome
+int expandAroundCenterPractice(string s, int i, int j)
+{
+    int count = 0;
+
+    while (i >= 0 && j < s.length() && s[i] == s[j])
+    {
+        //? is a substr palindrome sequence
+        count++;
+        i--;
+        j++; //? expanding for more matched sequences
+    }
+
+    return count;
+}
+
+int countSubstringsPractice(string s)
+{
+    int totalCount = 0;
+
+    //? every char of s string can be a center
+    for (int center = 0; center < s.length(); center++)
+    {
+        //? finding odd sequences
+        int i = center;
+        int j = center;
+        int oddCount = expandAroundCenterPractice(s, i, j);
+
+        //? finding even sequences
+        i = center;
+        j = center + 1;
+        int evenCount = expandAroundCenterPractice(s, i, j);
+
+        totalCount = totalCount + oddCount + evenCount;
+    }
+
+    return totalCount;
+}
+
 int main()
 {
     return 0;
