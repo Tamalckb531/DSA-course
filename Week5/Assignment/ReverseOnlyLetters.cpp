03fd5869c7a354be3636690b02bdf06bdc976c20
Tamal <ckbtamaldipnew@gmail.com>
2025-04-20 17:14:50 +0600

    Week5 (assignment) -> LeetCode 917. Reverse Only Letters -> Practice done

--- Week5/Assignment/ReverseOnlyLetters.cpp
+++ Week5/Assignment/ReverseOnlyLetters.cpp
@@ -27,6 +27,32 @@ string reverseOnlyLetters(string s)
     return s;
 }
 
+string reverseOnlyLetters(string s)
+{
+    int low = 0;
+    int high = s.length() - 1;
+
+    while (low <= high)
+    {
+        if (isalpha(s[low]) && isalpha(s[high]))
+        {
+            swap(s[low], s[high]);
+            low++;
+            high--;
+        }
+        else if (!isalpha(s[low]))
+        {
+            low++;
+        }
+        else
+        {
+            high--;
+        }
+    }
+
+    return s;
+}
+
 int main()
 {
     return 0;
