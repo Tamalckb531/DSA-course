5a9fa65e467e06aeab36450528c68d07bd10eff6
Tamal <ckbtamaldipnew@gmail.com>
2025-04-25 13:21:20 +0600

    Week5 (assignment) -> LeetCode 5. Longest Palindromic Substring -> Practice done

--- Week5/Assignment/LongestPalindromicSubstring.cpp
+++ Week5/Assignment/LongestPalindromicSubstring.cpp
@@ -38,6 +38,37 @@ string longestPalindrome(string s)
     return ans;
 }
 
+bool isPalindrome(string &s, int start, int end)
+{
+    while (start < end)
+    {
+        if (s[start] != s[end])
+            return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+string longestPalindrome(string s)
+{
+    string ans = "";
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        for (int j = i; j < s.size(); j++)
+        {
+            if (isPalindrome(s, i, j))
+            {
+                string palSubStr = s.substr(i, j - i + 1);
+                ans = palSubStr.size() > ans.size() ? palSubStr : ans; //? storing the longest palindromic sub string
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
 }
