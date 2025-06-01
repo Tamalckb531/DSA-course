83250911ca1708d96e7b0fa53b082f3521976777
Tamal <ckbtamaldipnew@gmail.com>
2025-06-01 16:25:56 +0600

    Array & Hashing -> LeetCode 1929: Concatenation of Array

--- neetcode250/ArrayHashing/ConcatOfArray1929.cpp
+++ neetcode250/ArrayHashing/ConcatOfArray1929.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+vector<int> getConcatenation(vector<int> &nums)
+{
+    vector<int> ans;
+    int size = nums.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        ans.push_back(nums[i]);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        ans.push_back(nums[i]);
+    }
+
+    return ans;
+}
 
 int main()
 {
