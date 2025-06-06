9cccb448ead4051d5592faadbf4d8e6fbdd98e62
Tamal <ckbtamaldipnew@gmail.com>
2025-06-06 15:59:23 +0600

    Array & Hashing -> LeetCode 49. Group Anagrams

---
+++ neetcode250/ArrayHashing/medium/groupAnagrams49.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string> &strs)
+{
+    unordered_map<string, vector<string>> map;
+    vector<vector<string>> ans;
+
+    for (string str : strs)
+    {
+        string key = str;
+        sort(key.begin(), key.end());
+        map[key].push_back(str);
+    }
+
+    for (auto entry : map)
+    {
+        ans.push_back(entry.second);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    cout << "LeetCode 706. Design HashMap" << endl;
+}
