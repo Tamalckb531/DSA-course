e34c5f7a5aa722be9bf9a335f0e03ccae7f4790b
Tamal <ckbtamaldipnew@gmail.com>
2025-07-12 21:06:06 +0600

    asfa

---
+++ neetcode250/TwoPointers/medium/3Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    //? Sort nums
+    sort(nums.begin(), nums.end());
+}
+
+int main()
+{
+    cout << "LeetCode 15. 3Sum" << endl;
+}
