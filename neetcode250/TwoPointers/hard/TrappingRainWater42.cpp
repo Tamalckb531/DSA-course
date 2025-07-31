014785fdbf44be34a6902b9323c4eca3a7d4532a
Tamal <ckbtamaldipnew@gmail.com>
2025-07-31 19:17:07 +0600

    Two Pointers -> Leetcode 42. Trapping Rain Water -> two pointer approach

--- neetcode250/TwoPointers/hard/TrappingRainWater42.cpp
+++ neetcode250/TwoPointers/hard/TrappingRainWater42.cpp
@@ -29,6 +29,38 @@ int trap(vector<int> &height)
 
     return waterTrapped;
 }
+
+//? Prefix-Suffix -> TC:O(n), SC:O(n)
+int trap(vector<int> &height)
+{
+    if (height.empty())
+        return 0;
+    int n = height.size() - 1;
+    int waterTrapped = 0;
+    vector<int> LeftMax(n + 1);
+    vector<int> RightMax(n + 1);
+
+    LeftMax[0] = height[0];
+    for (int i = 1; i <= n; i++)
+    {
+        LeftMax[i] = max(height[i], LeftMax[i - 1]);
+    }
+
+    RightMax[n] = height[n];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        RightMax[i] = max(height[i], RightMax[i + 1]);
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        int leftWall = LeftMax[i];
+        int rightWall = RightMax[i];
+        waterTrapped += min(leftWall, rightWall) - height[i];
+    }
+
+    return waterTrapped;
+}
 int main()
 {
     cout << "LeetCode 42. Trapping Rain Water" << endl;
