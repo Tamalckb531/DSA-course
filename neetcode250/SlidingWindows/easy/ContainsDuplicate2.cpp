9de566b7c35b02727120eaa1bbc8e76d30f7dcfe
Tamal <ckbtamaldipnew@gmail.com>
2025-08-01 12:00:18 +0600

    Sliding Windows -> Easy -> Leetcode 219. Contains Duplicate II -> set with sliding window optimized

--- neetcode250/SlidingWindows/easy/ContainsDuplicate2.cpp
+++ neetcode250/SlidingWindows/easy/ContainsDuplicate2.cpp
@@ -18,6 +18,25 @@ bool containsNearbyDuplicate(vector<int> &nums, int k)
 
     return false;
 }
+
+//? Sliding Window: TC:O(n), SC:O(k);
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    unordered_set<int> window;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (window.count(nums[i]))
+            return true;
+        window.insert(nums[i]);
+
+        //? Sliding the window by deleting the i-k th value from the nums array
+        if (window.size() > k)
+            window.erase(nums[i - k]);
+    }
+
+    return false;
+}
 int main()
 {
     cout << "LeetCode 219. Contains Duplicate II" << endl;
