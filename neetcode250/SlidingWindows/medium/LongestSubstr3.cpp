213cdcbe7b3f9afb7056b4204faf0a68e1eb326a
Tamal <ckbtamaldipnew@gmail.com>
2025-08-02 23:07:28 +0600

    Sliding Windows -> Easy -> Leetcode 3. Longest Substring Without Repeating Characters -> Sliding window

--- neetcode250/SlidingWindows/medium/LongestSubstr3.cpp
+++ neetcode250/SlidingWindows/medium/LongestSubstr3.cpp
@@ -23,6 +23,27 @@ int lengthOfLongestSubstring(string s)
 
     return longestSubstr;
 }
+
+//? Sliding window : TC:O(n), SC:O(s.size())
+int lengthOfLongestSubstring(string s)
+{
+    int ans = 0;
+    unordered_set<char> charSet;
+    int left = 0;
+
+    for (int right = 0; right < s.size(); right++)
+    {
+        while (charSet.find(s[right]) != charSet.end())
+        {
+            charSet.erase(s[left]); //? removing from scratch to duplicate to maintain sub str
+            left++;                 //? sliding the window of sub string
+        }
+        charSet.insert(s[right]);
+        ans = max(ans, (right - left + 1));
+    }
+
+    return ans;
+}
 int main()
 {
     cout << "LeetCode 3. Longest Substring Without Repeating Characters" << endl;
