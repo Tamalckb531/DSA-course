a721e9a0b02223b4c20db168536edd7ad6a330c0
Tamal <ckbtamaldipnew@gmail.com>
2025-08-06 18:16:01 +0600

    Sliding Windows -> Hard -> Leetcode 567. Permutation in String -> Sliding window

--- neetcode250/SlidingWindows/medium/PermutationInString567.cpp
+++ neetcode250/SlidingWindows/medium/PermutationInString567.cpp
@@ -3,37 +3,80 @@
 using namespace std;
 
 //? Hash Table: TC:O(s1.size() * s2.size()), SC:O(1)
+// bool checkInclusion(string s1, string s2)
+// {
+//     unordered_map<char, int> s1Count;
+
+//     for (auto s : s1)
+//     {
+//         s1Count[s]++;
+//     }
+
+//     int left = 0;
+//     int right = s1.size() - 1;
+
+//     while (right < s2.size())
+//     {
+//         unordered_map<char, int> window;
+
+//         for (int i = left; i <= right; i++)
+//         {
+//             window[s2[i]]++;
+//         }
+
+//         if (window == s1Count)
+//             return true;
+
+//         left++;
+//         right++;
+//     }
+
+//     return false;
+// }
+
+//? Sliding Window: TC:O(n), SC:O(1)
 bool checkInclusion(string s1, string s2)
 {
+    if (s1.size() > s2.size())
+        return false;
+
+    int n = s1.size();
+    int m = s2.size();
+
     unordered_map<char, int> s1Count;
+    unordered_map<char, int> window;
 
     for (auto s : s1)
     {
         s1Count[s]++;
     }
 
-    int left = 0;
-    int right = s1.size() - 1;
-
-    while (right < s2.size())
+    for (int i = 0; i < n; i++)
     {
-        unordered_map<char, int> window;
+        window[s2[i]]++;
+    }
+    if (window == s1Count)
+        return true;
 
-        for (int i = left; i <= right; i++)
-        {
-            window[s2[i]]++;
-        }
+    int left = 0;
+
+    for (int right = n; right < m; right++)
+    {
+        window[s2[left]]--;
+        if (window[s2[left]] == 0)
+            window.erase(s2[left]);
+
+        window[s2[right]]++;
 
         if (window == s1Count)
             return true;
-
         left++;
-        right++;
     }
 
     return false;
 }
 int main()
 {
+    cout << checkInclusion("ab", "eidbaooo") << endl;
     cout << "LeetCode 567. Permutation in String" << endl;
 }
