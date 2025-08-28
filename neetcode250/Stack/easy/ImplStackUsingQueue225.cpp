cf48e239f64dfc62158aebebc7588ec2d6fdcb09
Tamal <ckbtamaldipnew@gmail.com>
2025-08-28 22:36:52 +0600

    as

---
+++ neetcode250/Stack/easy/ImplStackUsingQueue225.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class MyStack
+{
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+    }
+
+    int pop()
+    {
+    }
+
+    int top()
+    {
+    }
+
+    bool empty()
+    {
+    }
+};
+
+int main()
+{
+    cout << "LeetCode 225. Implement Stack using Queues" << endl;
+} //? nothing
